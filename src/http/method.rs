7a578749288547e89d7d86a4e24417038692fc73
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-19 11:54:57 +0100

    Moved http module into separate folder

---
+++ src/http/method.rs
@@ -0,0 +1,11 @@
+pub enum HttpMethod {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,10 @@
 use http::request::Request;
 use server::Server;
 
+mod http;
 mod server;
 
 fn main() {
     let mut server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-mod http {
-    pub mod request {
-        use super::method::HttpMethod;
-
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: HttpMethod,
-        }
-    }
-
-    pub mod method {
-        pub enum HttpMethod {
-            GET,
-            DELETE,
-            POST,
-            PUT,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH,
-        }
-    }
 }
