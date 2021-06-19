d09973d7c2751d7ab3a88d9897b2b4f804cfb17c
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-19 11:46:09 +0100

    Imported server file into main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
 use http::request::Request;
 use server::Server;
 
+mod server;
+
 fn main() {
     let mut server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-mod server {
-    pub struct Server {
-        address: String,
-    }
-
-    impl Server {
-        pub fn new(address: String) -> Self {
-            Self { address }
-        }
-
-        pub fn run(&mut self) {
-            println!("Server running on: {}!", self.address);
-        }
-    }
 }
 
 mod http {
