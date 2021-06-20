ee1bf1d0f25af2fea20b4df278d2aebba04e1655
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-20 12:39:53 +0100

    Converted buffer to string and handled error scenario

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,6 +2,7 @@ use super::method::HttpMethod;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str::{self, Utf8Error};
 
 pub struct Request {
     pub path: String,
@@ -13,6 +14,8 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buffer)?;
+
         unimplemented!()
     }
 }
@@ -44,5 +47,11 @@ impl Display for ParseError {
 impl Debug for ParseError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
