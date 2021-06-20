a1c2e0fc861727e68ea89bd6a474b1931e0c1f77
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-20 12:21:04 +0100

    Changed error type from String to ParseError

--- src/http/request.rs
+++ src/http/request.rs
@@ -10,7 +10,7 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!()
