e4f3c9bbfb3388f9e9faeecd1f55a086a9d4266d
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-20 11:42:22 +0100

    Started TryFrom trait to handle conversion from buffer to Request

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,22 @@
 use super::method::HttpMethod;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: HttpMethod,
+}
+
+impl Request {
+    fn from_bytes_array(buffer: &[u8]) -> Result<Self, String> {
+        unimplemented!()
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
 }
