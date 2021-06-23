421b402df670773d8b8063ed3f4b78132ae7a2d1
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-23 16:07:32 +0100

    Added 'reason_phrase' method for StatusCode enum

--- src/http/status_code.rs
+++ src/http/status_code.rs
@@ -3,3 +3,13 @@ pub enum StatusCode {
     BadRequest = 400,
     NotFound = 404,
 }
+
+impl StatusCode {
+    pub fn reason_phrase(&self) -> &str {
+        match self {
+            Self::Ok => "Ok",
+            Self::BadRequest => "Bad request",
+            Self::NotFound => "Not found",
+        }
+    }
+}
