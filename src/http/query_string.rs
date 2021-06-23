93a90097d7d5c6588d8b4a25430f1eeb98d95c4d
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-23 14:21:58 +0100

    Created From trait for QueryString struct

--- src/http/query_string.rs
+++ src/http/query_string.rs
@@ -14,3 +14,30 @@ impl<'buffer> QueryString<'buffer> {
         self.data.get(key)
     }
 }
+
+impl<'buffer> From<&'buffer str> for QueryString<'buffer> {
+    fn from(raw: &'buffer str) -> Self {
+        // a=1&b=2&c=3&a=4
+        let data = raw.split('&').fold(HashMap::new(), |mut map, pair| {
+            let mut iterator = pair.split('=');
+
+            match (iterator.next(), iterator.next()) {
+                (Some(key), Some(value)) => {
+                    map.entry(key)
+                        .and_modify(|existing| match existing {
+                            QueryParameterValue::Single(previous) => {
+                                *existing = QueryParameterValue::Multiple(vec![previous, value])
+                            }
+                            QueryParameterValue::Multiple(vec) => vec.push(value),
+                        })
+                        .or_insert(QueryParameterValue::Single(value));
+                }
+                _ => println!("Failed to parse key-value pair: '{}'", pair),
+            };
+
+            map
+        });
+
+        Self { data }
+    }
+}
