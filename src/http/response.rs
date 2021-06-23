bbec5214379064c9f6d46c8884ab7fad8a0f5461
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-23 18:20:31 +0100

    Implemented Display trait for Response struct

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,4 +1,5 @@
 use super::StatusCode;
+use std::fmt::{Display, Formatter, Result as FmtResult};
 
 #[derive(Debug)]
 pub struct Response {
@@ -9,5 +10,22 @@ pub struct Response {
 impl Response {
     fn new(status_code: StatusCode, body: Option<String>) -> Self {
         Self { status_code, body }
+    }
+}
+
+impl Display for Response {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        let body = match &self.body {
+            Some(body) => body,
+            None => "",
+        };
+
+        write!(
+            f,
+            "HTTP/1.1 {code} {reason}\r\n\r\n{body}",
+            code = self.status_code,
+            reason = self.status_code.reason_phrase(),
+            body = body,
+        )
     }
 }
