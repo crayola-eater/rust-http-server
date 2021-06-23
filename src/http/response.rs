d37d65262003bcbb4acff2fdcc5ad8fa5afc95a3
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-23 15:57:04 +0100

    Added initial 'new' associated function for Response struct

--- src/http/response.rs
+++ src/http/response.rs
@@ -3,4 +3,10 @@ pub struct Response {
     body: Option<String>,
 }
 
+impl Response {
+    fn new(status_code: StatusCode, body: Option<String>) -> Self {
+        Self { status_code, body }
+    }
+}
+
 pub enum StatusCode {}
