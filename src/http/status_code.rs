417d841cda01efb248eb19dc0b0729a2e64eb2e4
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-23 17:18:57 +0100

    Implemented Display trait for StatusCode

--- src/http/status_code.rs
+++ src/http/status_code.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
 pub enum StatusCode {
     Ok = 200,
     BadRequest = 400,
@@ -11,5 +13,11 @@ impl StatusCode {
             Self::BadRequest => "Bad request",
             Self::NotFound => "Not found",
         }
+    }
+}
+
+impl Display for StatusCode {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", *self as u16)
     }
 }
