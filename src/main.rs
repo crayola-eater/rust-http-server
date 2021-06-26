1921da42c6dbbeb6c012580dd70aa18d1b264483
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-26 12:50:09 +0100

    Added logic to get public path via environment variable

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 
 use request_handler::RequestHandler;
 use server::Server;
+use std::env;
 
 mod http;
 mod request_handler;
 mod server;
 
 fn main() {
+    let public_path =
+        env::var("PUBLIC_PATH").expect("No PUBLIC_PATH environment variable detected");
     let mut server = Server::new("127.0.0.1:8080".to_string());
     server.run(RequestHandler);
 }
