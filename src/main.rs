e740388e247e5e4edd120a163ce5b8c879ca42b0
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-26 13:06:53 +0100

    Added default path in case no PUBLIC_PATH environment variable is provided

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,9 @@ mod request_handler;
 mod server;
 
 fn main() {
-    let public_path =
-        env::var("PUBLIC_PATH").expect("No PUBLIC_PATH environment variable detected");
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("Server will initialise with public path: {}", public_path);
     let mut server = Server::new("127.0.0.1:8080".to_string());
     let handler = RequestHandler::new(public_path);
     server.run(handler);
