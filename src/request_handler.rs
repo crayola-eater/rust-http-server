07e8ba85e18b2207c21769ab54bfceb5442451f0
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-26 11:52:40 +0100

    Made RequestHandler an empty struct

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,6 +1,6 @@
 use super::http::{Request, Response, StatusCode};
 use super::server::Handler;
-pub struct RequestHandler {}
+pub struct RequestHandler;
 
 impl Handler for RequestHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
