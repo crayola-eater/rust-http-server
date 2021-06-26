3edc6e68a2bbff1131f7a1d06f9d365c49d0bda0
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-26 13:15:55 +0100

    Added 'read_file' method for RequestHandler struct

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,5 +1,6 @@
 use super::http::{HttpMethod, Request, Response, StatusCode};
 use super::server::Handler;
+use std::fs;
 pub struct RequestHandler {
     public_path: String,
 }
@@ -7,6 +8,12 @@ pub struct RequestHandler {
 impl RequestHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        let file_contents = fs::read_to_string(path);
+        file_contents.ok()
     }
 }
 
