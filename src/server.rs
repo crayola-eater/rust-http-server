72ff8ef04ff5edc60e6757674c47ba347233fe9c
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-26 10:30:34 +0100

    Added default implementation for 'handle_bad_request' for Handler trait

--- src/server.rs
+++ src/server.rs
@@ -7,7 +7,10 @@ use std::net::TcpListener;
 pub trait Handler {
     fn handle_request(&mut self, request: &Request) -> Response;
 
-    fn handle_bad_request(&mut self, error: &ParseError) -> Response;
+    fn handle_bad_request(&mut self, error: &ParseError) -> Response {
+        println!("Failed to parse request: {}", error);
+        Response::new(StatusCode::BadRequest, None)
+    }
 }
 
 pub struct Server {
