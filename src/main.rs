5fcd50601d70bb1f60b553776c0a6e8b270dd7a4
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-26 11:55:32 +0100

    Passed RequestHandler to 'server.run'

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use http::HttpMethod;
 use http::Request;
+use request_handler::RequestHandler;
 use server::Server;
 
 mod http;
@@ -10,5 +11,5 @@ mod server;
 
 fn main() {
     let mut server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    server.run(RequestHandler);
 }
