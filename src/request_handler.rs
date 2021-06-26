b8b5786485d3db0d21eba8a44877fb025210c86e
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-26 11:46:27 +0100

    Implemented Handler trait for RequestHandler struct

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,3 +1,9 @@
 use super::http::{Request, Response, StatusCode};
 use super::server::Handler;
 pub struct RequestHandler {}
+
+impl Handler for RequestHandler {
+    fn handle_request(&mut self, request: &Request) -> Response {
+        unimplemented!()
+    }
+}
