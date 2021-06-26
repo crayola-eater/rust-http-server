21f402130747469c3a4b550060a5993e03a81895
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-06-26 13:20:05 +0100

    Changed '/' route to serve index.html instead

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -21,7 +21,7 @@ impl Handler for RequestHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             &HttpMethod::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/status" => Response::new(StatusCode::Ok, Some("Server is running!".to_string())),
                 _ => Response::new(StatusCode::NotFound, None),
             },
